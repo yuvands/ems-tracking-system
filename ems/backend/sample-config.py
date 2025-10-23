6d2c4d777d24ce83819acaf3679952bd77df419b
Yuvan DS <yuvands@users.noreply.github.com>
2025-10-23 18:00:30 +0530

    changes made to sample-config

--- ems/backend/sample-config.py
+++ ems/backend/sample-config.py
@@ -4,7 +4,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'a-super-secret-key'
     
     # --- SQLAlchemy Database Configuration ---
-    # Add your own credentials here
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://USERNAME:PASSWORD@localhost/aether_ems_db'
     
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://USERNAME:PASSWORD@localhost/aether_ems_db'
+    # IMP: Replace username and password with your own creds
     SQLALCHEMY_TRACK_MODIFICATIONS = False
